2c00ffae8034b8b960c05db4c5ae85a2681e08a6
Mamie Smith <girlinmywhirl@gmail.com>
2018-05-20 21:44:20 -0400

    adding Heroku hook

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,17 @@
-require("dot-env");
 var mySQL = require("mysql");
-var connection = mySQL.createConnection({
-    host: "localhost",
-    port: 8080,
-    user: "root",
-    password: "process.env.DATABASE_PASSWORD",
-    dataabase: "burgerMenu"
-});
+var connection;
+if (process.env.JAWSDB_URL) {
+    connection = mySQL.createConnection(process.env.JAWSDB_URL);
+} else {
+    connecction = +mySQL.createConnection({
+        host: "localhost",
+        port: 8080,
+        user: "root",
+        password: "mickeymouse",
+        dataabase: "burgerMenu"
+    });
+}
 
-connection.connect(function(err) {
-    if (err) {
-        console.log(" UNABLE TO CONNECT TO DB: " + err.stack);
-        return
-    }
-    console.log("**** burgerMenu DB connection id: " + connection.threadId);
-});
+connection.connect();
 
 module.exports = connection;
